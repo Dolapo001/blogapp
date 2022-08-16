462c343169853179664865e170e86e12d2e04d4f
Dolapo24 <adedolapovictoria927@gmail.com>
2022-08-16 22:13:46 +0100

    new

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 from .models import Post
 # Create your views here.
 
@@ -12,3 +16,27 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
